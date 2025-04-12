6e4c035655859d0b033f565d04ba81ecdde7d534
Gaurav Pratap Singh <gs492979@gmail.com>
2025-04-12 15:24:31 +0530

    rolling dice functionality added.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+
+//Selecting Elements
+const player1ScoreElem = document.getElementById("score--0");
+const player2ScoreElem = document.getElementById("score--1");
+const diceElem = document.querySelector(".dice");
+const newGameBtn = document.querySelector(".btn--new");
+const rollDiceBtn = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
+
+//Initial State
+player1ScoreElem.textContent = 0;
+player2ScoreElem.textContent = 0;
+diceElem.classList.add("hidden");
+
+//Rolling dice functionality
+rollDiceBtn.addEventListener("click", function () {
+  const diceValue = Math.trunc(Math.random() * 6) + 1;
+  diceElem.classList.remove("hidden");
+  diceElem.src = `./images/dice-${diceValue}.png`;
+  //diceElem.setAttribute("src", `./images/dice-${diceValue}.png`);
+});
